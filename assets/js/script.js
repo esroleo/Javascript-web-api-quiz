a0452ba2440332bc13225ddec34cef7afbc1d785
Esteban Romero <esroleo@gmail.com>
2020-12-29 14:04:53 -0500

    feature button logic answers completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var quizStatus = false; // Know the status of the quiz. Quiz is not running = false , running = true
+var questionNumber = 0; // Track the question answered.
+var answerNumber = 0; // Track next answers to show
 var score = 0; // Score tracker
 var startQuizBtnEl = document.getElementById('start-quiz'); // Start Quiz button El
 var answer1BtnEl = document.getElementById('answer1'); // Start Quiz button El
@@ -10,7 +12,7 @@ var mainDivEl = document.getElementById('mainDiv'); // Main div container for al
 var htmlTimeLeft = document.getElementById('timeLeft'); // Display counter @ the html level.
 var questionDisplayEl = document.createElement("questionDisplay"); // Display Question
 var button1234 = document.createElement("button"); // Test answer 1
-var timeLeft = 5; // Global time left assignment counter
+var timeLeft = 60; // Global time left assignment counter
 
 answer1BtnEl.style.display = 'none';
 answer2BtnEl.style.display = 'none';
@@ -23,42 +25,146 @@ var questionsObject = { // Object that holds correct answers.
     correct: { 
         0 : "Commonly used datatypes DO NOT include?",
         1 : "The condition statement if/else is enclosed with the following:",
-        2 : "Arrays can be used to store the following",
-        3 : "A very useful tool to debug arrays is:",
+        2 : "Arrays can be used to store the following", // Button #4 for 
+        3 : "A very useful tool to debug arrays is:", // Button #3
         4 : "Strings must be enclosed with:"
     }
-}
+};
 
+var answersObject = { // Object that holds correct answers.
+    answers: { 
+        0 : {
+            0: "Strings",
+            1: "Boolean",
+            2: "Alerts",
+            3: "Numbers"},
+        1 : {
+            0: "Parentheses",
+            1: "Curly Brackets",
+            2: "Quotes",
+            3: "Square Brackets"},
+        2 : { // Button #3
+            0: "Javascript",
+            1: "Terminal/bash",
+            2: "For loops", 
+            3: "Console.log"},      
+        3 : { // Answer to question 5 --> Button #2
+            0: "Commas",
+            1: "Curly brackets",
+            2: "Quotes", 
+            3: "Parentheses"},      
+        4 : { // Button #4
+            0: "Number of strings",
+            1: "Other arrays",
+            2: "Booleans",
+            3: "All of the above"},  
+    }
+};
 
 
 htmlTimeLeft.textContent = timeLeft;
 
 startQuizBtnEl.addEventListener("click", function() {
+
 //debugger;
     startQuizBtnEl.style.display = 'none';
     questionDisplay.style.display='none';
-    timeLeft=5;
+    timeLeft=60;
     htmlTimeLeft.textContent = timeLeft;
-    
 
+ //debugger;
+
+   
+    
     var timeInterval = setInterval(function() {
         if(timeLeft >= 1) {
             //questionDisplayEl.textContent = questionsObject.correct[0];
             //mainDivEl.appendChild(questionDisplayEl);
             //mainDivEl.appendChild(button1234);
             //questionDisplay.style.display= "";
-            questionDisplay.textContent = questionsObject.correct[0];
+            questionDisplay.textContent = questionsObject.correct[questionNumber];
             questionDisplay.style.display= "";
             answer1BtnEl.style.display = "";
             answer2BtnEl.style.display = "";
             answer3BtnEl.style.display = "";
             answer4BtnEl.style.display = "";
-            answer1BtnEl.textContent = "Edit";
+         
+            answer1BtnEl.textContent = answersObject.answers[answerNumber][0];
+            answer2BtnEl.textContent = answersObject.answers[answerNumber][1];
+            answer3BtnEl.textContent = answersObject.answers[answerNumber][2];
+            answer4BtnEl.textContent = answersObject.answers[answerNumber][3];
+           
             gridContainer.appendChild(questionDisplayEl);
             gridContainer.appendChild(answer1BtnEl);
             timeLeft -= 1;
             htmlTimeLeft.textContent = timeLeft;
             console.log("time left:" + timeLeft)
+
+            answer1BtnEl.addEventListener("click", function() {
+
+                if (questionDisplay.textContent === "The condition statement if/else is enclosed with the following:" && answer1BtnEl.textContent === "Parentheses") {
+                    console.log("Correct");
+                   // timeLeft += 1; // Add a second for a correct answer as it will take one second to move to the next question
+                    score += 10; // Give user a 10+ score
+                    questionNumber = 2; // Move to the next question which is the third questions
+                    answerNumber = 4;
+                }
+      
+
+            });
+
+            answer2BtnEl.addEventListener("click", function() {
+
+                if (questionDisplay.textContent === "Strings must be enclosed with:" && answer2BtnEl.textContent === "Curly brackets") {
+                    console.log("Correct");
+                    //timeLeft += 1; // Add a second for a correct answer as it will take one second to move to the next question
+                    score += 10; // Give user a 10+ score
+                    //questionNumber = 2; // Move to the next question
+                    //game over
+                    window.alert("Game Over");
+                }
+
+                
+            });
+
+            answer3BtnEl.addEventListener("click", function() {
+
+                if (questionDisplay.textContent === "Commonly used datatypes DO NOT include?" && answer3BtnEl.textContent === "Alerts") {
+                    console.log("Correct");
+                    //timeLeft += 1; // Add a second for a correct answer as it will take one second to move to the next question
+                    score += 10; // Give user a 10+ score
+                    questionNumber = 1; // Move to the next question which is the second question
+                    answerNumber = 1;
+                }
+
+                else if (questionDisplay.textContent === "A very useful tool to debug arrays is:" && answer3BtnEl.textContent === "For loops") {
+                    console.log("Correct");
+                    //timeLeft += 1; // Add a second for a correct answer as it will take one second to move to the next question
+                    score += 10; // Give user a 10+ score
+                    questionNumber = 4; // Move to the next question which  is the fifth question
+                    answerNumber =3;
+                }
+
+            
+
+
+                
+            });
+
+            answer4BtnEl.addEventListener("click", function() {
+
+                if (questionDisplay.textContent === "Arrays can be used to store the following" && answer4BtnEl.textContent === "All of the above") {
+                    console.log("Correct");
+                   // timeLeft += 1; // Add a second for a correct answer as it will take one second to move to the next question
+                    score += 10; // Give user a 10+ score
+                    //Game is overquestionNumber = 4; // Move to the next question
+                    questionNumber = 3; // Move to the next question which is the fourth question
+                    answerNumber = 2;
+
+                }
+                
+            });
+
         }
         else if(timeLeft === 0){
           //timerEl.textContent = "";
@@ -68,6 +174,7 @@ startQuizBtnEl.addEventListener("click", function() {
           answer2BtnEl.style.display = 'none';
           answer3BtnEl.style.display = 'none';
           answer4BtnEl.style.display = 'none';
+         
           questionDisplay.textContent = "Game Over!. Try again by clicking on \"Click Start Quiz\"";
           startQuizBtnEl.style.display = "";
           //gridContainer.appendChild(questionDisplayEl);
@@ -75,6 +182,9 @@ startQuizBtnEl.addEventListener("click", function() {
           //displayMessage();
         }
       }, 1000)
+
+    
+
 
 /*
 
